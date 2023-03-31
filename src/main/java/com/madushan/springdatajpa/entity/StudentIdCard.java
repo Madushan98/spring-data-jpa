6b33f7d26c76932f39be2b8d70cd1c884699d40b
madushan <ranasinhemadushan@gmail.com>
2023-03-31 18:00:22 +0530

    Student entity created

---
+++ src/main/java/com/madushan/springdatajpa/entity/StudentIdCard.java
@@ -0,0 +1,77 @@
+package com.madushan.springdatajpa.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.UniqueConstraint;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+
+@Entity(name = "StudentIdCard")
+@Table(
+        name = "student_id_card",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "student_id_card_number_unique",
+                        columnNames = "card_number"
+                )
+        }
+)
+public class StudentIdCard {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_card_id_sequence",
+            sequenceName = "student_card_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_card_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "card_number",
+            nullable = false,
+            length = 15
+    )
+    private String cardNumber;
+
+    @OneToOne(mappedBy = "studentIdCard")
+    private Student student;
+
+    public StudentIdCard(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public StudentIdCard(String cardNumber, Student student) {
+        this.cardNumber = cardNumber;
+        this.student = student;
+    }
+
+    public StudentIdCard() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentIdCard{" +
+                "id=" + id +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", student=" + student +
+                '}';
+    }
+
+}
